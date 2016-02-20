d8f4c07ad777001c549aa437ba7e926eed9a802c
Kevin Peters <kwpeters@gmail.com>
2016-02-19 21:49:52 -0500

    Started implementing PhotoLibrary import() unit tests.

--- src/photoLibrary.spec.js
+++ src/photoLibrary.spec.js
@@ -7,7 +7,8 @@
 
 
 var PhotoLibrary = require("./photoLibrary"),
-    Directory = require("./directory");
+    Directory    = require("./directory"),
+    File         = require("./file");
 
 describe("PhotoLibrary", function () {
     "use strict";
@@ -25,11 +26,26 @@ describe("PhotoLibrary", function () {
 
         describe("import()", function () {
 
+            var tmpDir = new Directory("tmp"),
+                libDir = new Directory(tmpDir, "photo-library"),
+                srcDir = new Directory(tmpDir, "source-photos");
+
+            tmpDir.emptySync();
+            libDir.ensureExistsSync();
+
+            new File("./test/input/2015-03-11 09.05.32.jpg").copySync(srcDir);
+            new File("./test/input/IMG_8718-canon.JPG").copySync(srcDir);
+            new File("./test/input/P1040165-panasonic.JPG").copySync(srcDir);
+
+            process.exit(-1);
+
 
             beforeEach(function () {
                 // Make sure the tmp folder is setup so we have one directory to
                 // import from and another directory to serve as the PhotoLibrary root
                 // directory.
+
+                destDir.emptySync();
             });
 
 
