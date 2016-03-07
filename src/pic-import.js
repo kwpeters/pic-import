a9be6452b1f3a8e2599f2ce0ce2c4b1ac8d6ea8d
Kevin Peters <kwpeters@gmail.com>
2016-03-07 08:30:07 -0500

    Deleted commented code.

--- src/pic-import.js
+++ src/pic-import.js
@@ -13,21 +13,6 @@ var path         = require("path"),
         .argv;
 
 
-//function oldmain() {
-//    var libraryRoot    = ".",
-//        imageFiles     = [
-//            "test/input/2015-03-11 09.05.32.jpg",
-//            "test/input/IMG_8718-canon.JPG"
-//        ];
-//
-//    imageFiles.map(function (imageFile) {
-//        exifReader.getCreateDate(imageFile).then(function (creationDate) {
-//            console.log("File:", imageFile, "Created:", creationDate);
-//        });
-//    });
-//}
-
-
 function main() {
     "use strict";
 
